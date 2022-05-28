e52c09e200b4bba49d8acb730b0145acaf5713b6
ojasgujju <77567127+ojasgujju@users.noreply.github.com>
2022-05-29 00:14:23 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: "AIzaSyBD921J26IUcqOe4NKHk4YSJv4aLZgwnd8",
+  authDomain: "test-64cae.firebaseapp.com",
+  databaseURL: "https://test-64cae.firebaseio.com",
+  projectId: "test-64cae",
+  storageBucket: "test-64cae.appspot.com",
+  messagingSenderId: "854975524198",
+  appId: "1:854975524198:web:605e1d2ce5d906213eaca7",
+  measurementId: "G-V4E9P91446"
 };
 
 // Initialize Firebase
